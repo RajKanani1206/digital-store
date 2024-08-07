7fe01c00ef8ae9f725031bf258e8fe866ba9ac29
Raj Kanani <rajkanani1206@gmail.com>
2024-08-07 16:52:25 +0200

    Initial commit

---
+++ client/pages/index.js
@@ -0,0 +1,28 @@
+import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
+import NewProducts from "@/components/NewProducts";
+
+export default function HomePage({ featuredProduct, newProducts }) {
+  return (
+    <div>
+      <Header />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "66b2d132b1297edc4041f9e7";
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, { sort: { _id: -1 }, limit: 10 });
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
+  };
+}
